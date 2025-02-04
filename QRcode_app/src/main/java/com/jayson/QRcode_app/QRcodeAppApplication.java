d7b8454231202cf2709e86c9afe4c3a1c9dc17d9
Jayson White <118446505+JaysonWhite13@users.noreply.github.com>
2025-02-04 10:48:13 +0100

    Add files via upload

---
+++ QRcode_app/src/main/java/com/jayson/QRcode_app/QRcodeAppApplication.java
@@ -0,0 +1,13 @@
+package com.jayson.QRcode_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QRcodeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QRcodeAppApplication.class, args);
+	}
+
+}
---
+++ QRcode_app/src/test/java/com/jayson/QRcode_app/QRcodeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayson.QRcode_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QRcodeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
